058bc254f90b887a79a0053dcc674f92b37ac009
rylynn-abo <rylynn_ab@163.com>
2018-11-19 09:41:38 +0800

    2018.11.19 9:41

--- src/main/java/edu/rylynn/storm/log/LogGenerator.java
+++ src/main/java/edu/rylynn/storm/log/LogGenerator.java
@@ -4,13 +4,22 @@ import edu.rylynn.storm.log.entity.Order;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.concurrent.TimeUnit;
+
 public class LogGenerator {
     private static final Logger logger = LogManager.getLogger(LogGenerator.class);
 
-    public void logSomething(){
-
+    public void logOrder(Order order) {
+        logger.info(order);
     }
+
     public static void main(String[] args) {
-        new LogGenerator().logSomething();
+        while (true) {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
